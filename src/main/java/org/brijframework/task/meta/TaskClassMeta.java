08ad0b7786d7609f166fcf3a703f43912c05492f
ram kishor <ramkishor0509@gmail.com>
2019-06-18 21:44:06 +0530

    ok

--- src/main/java/org/brijframework/task/meta/TaskClassMeta.java
+++ src/main/java/org/brijframework/task/meta/TaskClassMeta.java
@@ -1,16 +1,10 @@
 package org.brijframework.task.meta;
 
-import java.util.Map;
-
 import org.brijframework.meta.asm.AbstractClassMeta;
 import org.brijframework.meta.reflect.ConstMeta;
 import org.brijframework.meta.reflect.FieldMeta;
 
 public class TaskClassMeta extends AbstractClassMeta{
-
-	@Override
-	public void papulate(Map<String, Object> map) {
-	}
 
 	@Override
 	public FieldMeta getPropertyInfo(String _key) {
